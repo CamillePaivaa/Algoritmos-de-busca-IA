365e9aed2a4559ddc0bbe768ec6caa8a3e09e388
camille <camille.paiva@ufv.br>
2024-11-03 14:37:41 -0300

    Ajustes

--- agente.py
+++ agente.py
@@ -6,6 +6,8 @@ def posicionar_movimentar_agente(fronteira_largura, tabuleiro, posicao_x, posica
     tabuleiro[posicao_x][posicao_y] = agente # Colocar o agente na posição inicial
     destino_x = 6
     destino_y = 10
+    conjunto_explorado = set()
+
     
     # Verificar para onde o agente pode se mover
     # Seguindo esta ordem: ↑ cima, ← esquerda, → direita e ↓ baixo.
@@ -17,37 +19,43 @@ def posicionar_movimentar_agente(fronteira_largura, tabuleiro, posicao_x, posica
         posicao_direita = posicao_y + 1  
         posicao_baixo = posicao_x + 1  
 
-        if posicao_acima >= 0 and tabuleiro[posicao_acima][posicao_y] == 1:  
-            print('Agente pode se mover para cima.')
-            fronteira_largura.append((posicao_acima, posicao_y))
-        
-        if posicao_esquerda >= 0 and tabuleiro[posicao_x][posicao_esquerda] == 1:
-            print('Agente pode se mover para a esquerda.')
-            fronteira_largura.append((posicao_x, posicao_esquerda))
-
-        if posicao_direita < len(tabuleiro[0]) and tabuleiro[posicao_x][posicao_direita] == 1:
-            print('Agente pode se mover para a direita.')
-            fronteira_largura.append((posicao_x, posicao_direita)) #adicionando na ultima posição da fila
-
-        if posicao_baixo < len(tabuleiro) and tabuleiro[posicao_baixo][posicao_y] == 1:
-            print('Agente pode se mover para baixo.')
-            fronteira_largura.append((posicao_baixo, posicao_y))
-
-
-        print('Fronteira atualizada:', fronteira_largura)
-
-        #Andar com agente seguindo a primeira posição da fronteira
-    
+        #Andar com agente seguindo a primeira posição da fronteira 
         if fronteira_largura:
+            conjunto_explorado.add((posicao_x, posicao_y))  # Adicionar posição ao conjunto explorado
             posicao_x, posicao_y = fronteira_largura.popleft()  # Obter a nova posição
             tabuleiro[posicao_x][posicao_y] = agente  # Atualizar posição do agente no tabuleiro
             print('Fronteira final: ', fronteira_largura)
+            print('Conjunto explorado: ', conjunto_explorado)
+
             print('Tabuleiro com movimento do agente:')
 
             for linha in tabuleiro:
                 print(linha)
 
     
+
+        if posicao_acima >= 0 and tabuleiro[posicao_acima][posicao_y] == 1:
+            print('Agente pode se mover para cima.')
+            fronteira_largura.append((posicao_acima, posicao_y))
+           
+        if posicao_esquerda >= 0 and tabuleiro[posicao_x][posicao_esquerda]== 1:
+            print('Agente pode se mover para a esquerda.')
+            fronteira_largura.append((posicao_x, posicao_esquerda))
+            
+        if posicao_direita < len(tabuleiro[0]) and tabuleiro[posicao_x][posicao_direita]== 1:
+            print('Agente pode se mover para a direita.')
+            fronteira_largura.append((posicao_x, posicao_direita)) #adicionando na ultima posição da fila
+           
+
+        if posicao_baixo < len(tabuleiro) and tabuleiro[posicao_baixo][posicao_y]== 1:
+            print('Agente pode se mover para baixo.')
+            fronteira_largura.append((posicao_baixo, posicao_y))
+            
+
+
+        print('Fronteira atualizada:', fronteira_largura)
+      
+
     
 
     
