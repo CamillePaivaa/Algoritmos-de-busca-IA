0068b16139a951941875d3ab90526829cb3ad2c2
camille <camille.paiva@ufv.br>
2024-11-11 20:07:38 -0300

    Ajuste no menu, inserindo musica no jogo

--- interface_grafica.py
+++ interface_grafica.py
@@ -14,21 +14,25 @@ opcoes_busca = ["Busca em Profundidade", "Busca em Largura","Sair"]
 tipo_busca_selecionado = None
 
 def menu_selecao_busca():
-
+    import pygame
+    import sys
     from busca_largura import busca_em_largura
     from busca_profundidade import busca_em_profundidade
     global tipo_busca_selecionado
+    
     pygame.init()
+    pygame.mixer.init()
    
-
     tela = pygame.display.set_mode((largura_tela, altura_tela))
     pygame.display.set_caption("Menu de Seleção de Busca")
     font = pygame.font.Font("./font/RockSalt-Regular.ttf", 25)  
     selected_option = 0
 
+    # Carregar e iniciar a música
+    pygame.mixer.music.load("./assets/music/Ori and the Blind Forest – Main Theme [Menu Music].mp3")
+    pygame.mixer.music.play(-1)  # -1 para tocar em loop
 
-
-    imagem_fundo = pygame.image.load("./img/fundo.jpg") 
+    imagem_fundo = pygame.image.load("./assets/img/fundo.jpg") 
     imagem_fundo = pygame.transform.scale(imagem_fundo, (largura_tela, altura_tela))  
 
     espaco_entre_botoes = 20  # Espaço entre os botões
@@ -56,6 +60,8 @@ def menu_selecao_busca():
                     elif tipo_busca_selecionado == "Sair":
                         pygame.quit()
                         sys.exit()
+                        
+                    pygame.mixer.music.stop()
                     pygame.time.wait(2000) 
                     mensagem_vitoria()
                     selected_option = 0
@@ -63,11 +69,9 @@ def menu_selecao_busca():
         tela.fill((0, 0, 0))  
         tela.blit(imagem_fundo, (0, 0))  
 
-        # Desenha o título
         title_text = font.render("Selecione o Tipo de Busca", True, (255, 255, 255))
         tela.blit(title_text, (largura_tela // 2 - title_text.get_width() // 2, 50))
 
-      
         for i, option in enumerate(opcoes_busca):
             option_text = font.render(option, True, (255, 255, 255))
             button_width = option_text.get_width() + 20  
@@ -78,21 +82,21 @@ def menu_selecao_busca():
             button_color = (100, 100, 255) if i == selected_option else (50, 50, 50)
             pygame.draw.rect(tela, button_color, (button_x, button_y, button_width, button_height), border_radius=10)
 
-    
             tela.blit(option_text, (button_x + (button_width - option_text.get_width()) // 2,
                                 button_y + (button_height - option_text.get_height()) // 2))
 
         pygame.display.flip()
 
 
-imagem_labirinto = pygame.image.load('./img/map.png')  
+
+imagem_labirinto = pygame.image.load('./assets/img/map.png')  
 imagem_labirinto = pygame.transform.scale(imagem_labirinto, (TAMANHO_CELULA * COLUNAS_TABULEIRO, TAMANHO_CELULA * LINHAS_TABULEIRO))
 
 def desenhar_tabuleiro(tela, agente_pos,num_passos ):
     
     font = pygame.font.SysFont('arial', 24)
     texto_passos = font.render(f"    Número de Passos: {num_passos}", True, (0,0,0))
-    agente_img = pygame.image.load('./img/agente_maca.png')
+    agente_img = pygame.image.load('./assets/img/agente_maca.png')
 
     largura_tabuleiro = (TAMANHO_CELULA) * COLUNAS_TABULEIRO  
     altura_tabuleiro = (TAMANHO_CELULA) * LINHAS_TABULEIRO    
